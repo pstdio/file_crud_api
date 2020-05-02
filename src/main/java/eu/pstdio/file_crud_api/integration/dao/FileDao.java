757773cf5597392577bb69de8553ca064db0c029
mbogudal <m.bogudal@linux.pl>
2020-05-02 16:43:10 +0200

    fix

--- src/main/java/eu/pstdio/file_crud_api/integration/dao/FileDao.java
+++ src/main/java/eu/pstdio/file_crud_api/integration/dao/FileDao.java
@@ -1,6 +1,7 @@
 package eu.pstdio.file_crud_api.integration.dao;
 
 import eu.pstdio.file_crud_api.business.message.DeleteInputMessage;
+import eu.pstdio.file_crud_api.business.message.GetInputMessage;
 import eu.pstdio.file_crud_api.business.message.PostInputMessage;
 import eu.pstdio.file_crud_api.business.message.PutInputMessage;
 import java.io.InputStream;
@@ -12,8 +13,8 @@ import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
  * @author mib
  */
 public interface FileDao {
-    void create(PostInputMessage msg, FormDataContentDisposition fileMetadata, InputStream inputStream);
-    StreamingOutput getByGetInputMessage();
+    String create(PostInputMessage msg, FormDataContentDisposition fileMetadata, InputStream inputStream);
+    StreamingOutput getByGetInputMessage(GetInputMessage msg);
     void update(PutInputMessage msg, InputStream inputStream);
     void delete(DeleteInputMessage msg);
 }
